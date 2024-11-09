490510c6b89fc5f6b819db37edc5a50a8d8a2033
felipesinn <felipesinn01@gmail.com>
2024-11-09 16:53:16 -0300

    add handleLinkClick

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,12 +3,15 @@ import { HeaderStyle, H1, Nav } from './styles';
 import { Link } from 'react-router-dom';
 import { FaHome, FaTools, FaEnvelope, FaProjectDiagram, FaUser } from 'react-icons/fa'; 
 
-
 export const Header: React.FC = () => {
   const [showMenu, setShowMenu] = useState(false); 
 
   const toggleMenu = () => {
     setShowMenu((prev) => !prev); 
+  };
+
+  const handleLinkClick = () => {
+    setShowMenu(false); // Fecha o menu ao clicar em um link
   };
 
   return (
@@ -21,11 +24,11 @@ export const Header: React.FC = () => {
       <Nav showMenu={showMenu}>
         <button onClick={toggleMenu}>☰</button> 
         <ul>
-          <li><Link to="/"><FaHome /> Home</Link></li>
-          <li><Link to="/habilidades"><FaTools /> Habilidades</Link></li>
-          <li><Link to="/contato"><FaEnvelope /> Contato</Link></li>
-          <li><Link to="/projetos"><FaProjectDiagram /> Projetos</Link></li>
-          <li><Link to="/sobre"><FaUser /> Sobre</Link></li>
+          <li><Link to="/" onClick={handleLinkClick}><FaHome /> Home</Link></li>
+          <li><Link to="/habilidades" onClick={handleLinkClick}><FaTools /> Habilidades</Link></li>
+          <li><Link to="/contato" onClick={handleLinkClick}><FaEnvelope /> Contato</Link></li>
+          <li><Link to="/projetos" onClick={handleLinkClick}><FaProjectDiagram /> Projetos</Link></li>
+          <li><Link to="/sobre" onClick={handleLinkClick}><FaUser /> Sobre</Link></li>
         </ul>
       </Nav>
     </HeaderStyle>
